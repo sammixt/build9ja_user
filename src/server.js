5e057811b575fd028d4001b9ff032d2c282e3a32
sezealaflw <samuel.ezeala@flutterwavego.com>
2023-05-16 15:19:55 +0100

    heroku switch

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-export const server = "https://build9ja-backend.vercel.app/api/v2";
+export const server = "https://build9ja-api.herokuapp.com/api/v2";
 //export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "https://build9ja-backend.vercel.app/"
+export const backend_url = "https://build9ja-api.herokuapp.com/"
 //export const backend_url = "http://localhost:8000/"
 
 export const socket_url = "https://build9ja-socket.vercel.app//"
