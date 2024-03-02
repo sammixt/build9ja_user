c83cbd2d52b21f94908c81b81cbb746534116c17
Joy <oyinyeezeala@Joys-MacBook-Air.local>
2024-03-02 16:25:00 +0100

    heroku taken out

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
-// export const server = "https://build9ja-api.herokuapp.com/api/v2";
 export const server = "https://build9ja-backend.vercel.app/api/v2";
 
-// export const backend_url = "https://build9ja-api.herokuapp.com/"
 export const backend_url = "https://build9ja-backend.vercel.app/"
 
-// export const socket_url = "https://build9ja-socket.vercel.app//"
 export const socket_url = "http://localhost:4000/"
